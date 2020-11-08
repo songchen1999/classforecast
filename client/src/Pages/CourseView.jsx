1cf2652ebbe455f13f4bb471421faff0cd3247a5
tjgoldblatt <tj@psyton.com>
2020-11-07 19:04:06 -0500

    Added templates for webpages and routing for links to webpages

---
+++ client/src/Pages/CourseView.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import {BrowserRouter as Router, Link, Route, Switch} from 'react-router-dom';
+
+export class CourseView extends React.Component {
+  state = {};
+
+  render() {
+    return (
+      <div>
+        <h2>This is the CourseView webpage</h2>
+      </div>
+    );
+  }
+}
+
+export default CourseView;
