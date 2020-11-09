7399dee56a90eb980c39fd8ece56ff827f1165d6
Song Chen <43616275+songchen1999@users.noreply.github.com>
2020-11-09 12:54:18 -0500

    Update course schema

--- models/Course.js
+++ models/Course.js
@@ -1,17 +1,28 @@
+// classes from course offering plan
 const mongoose = require('mongoose');
 const {Schema} = mongoose;
 
 const CourseSchema = new Schema({
+    // CICS or CompSci
     college: {
         type: String,
     },
+    // eg 325
     number: {
         type: String
     },
-    level: {
+    // eg 3
+    credits:{
         type: String
-    }
-    
+    },
+    // eg Intro to HCI
+    title: {
+        type: String
+    },
+    // spring and fall
+    semester: {
+        type: String
+    }  
 });
 
 const Course = mongoose.model('courses',CourseSchema);
