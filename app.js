7b2bf58ce5ca36aa58c1dd8dbb8db81c2a243f4b
Song Chen <43616275+songchen1999@users.noreply.github.com>
2020-11-09 13:51:42 -0500

    testdatabaserunning

--- app.js
+++ app.js
@@ -8,14 +8,28 @@ const mongoURI = process.env.MONGOURL;
 mongoose.connect(mongoURI,{useNewUrlParser:true, useUnifiedTopology:true})
 .then(()=>{console.log('DB connected')})
 .catch((err)=>{console.log(err)});
+const Course = require('./models/Course');
+
+
 // testing 
+app.get('/',(req,res)=>{
+  res.send('Running');
+})
+
 app.get('/instructors', (req, res) => {
   res.send('instructors')
 })
 
 app.get('/courses',(req,res)=>{
+  Course.find({}).then(
+    a=>{
+      res.send(a);
+    }).catch((e)=>{
+      res.send(e);
+    })
   
 })
+
 
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`)
