b47829ae8cc6cfce299ba6d2f052d45b73f88cc3
Charlie <61515720+cbouhanna-umass@users.noreply.github.com>
2020-11-10 15:24:24 -0500

    Homepage ANT, text, links
    Installed ANT design
    Added the required texts
    Incomplete: buttons and dividers for semesters

--- client/src/Pages/HomeView.jsx
+++ client/src/Pages/HomeView.jsx
@@ -1,5 +1,8 @@
 import React from 'react';
+import ReactDOM from 'react-dom';
 import {BrowserRouter as Router, Link, Route, Switch} from 'react-router-dom';
+import { Button, Typography, Divider } from 'antd';
+//const { Title, Paragraph, Text, Link } = Typography;
 
 export class HomeView extends React.Component {
   state = {};
@@ -8,11 +11,26 @@ export class HomeView extends React.Component {
     return (
       <Router>
         <div>
-          <h2>This is the HomeView webpage</h2>
+          <h2>UMass CICS Course Scheduler</h2>
+          <h2>Overview of Semesters</h2>
         </div>
+        <p>
+          Spring 2021
+        </p>
+        <Button href="semester/spring2021">View</Button>
+        <Divider />
+        <p>
+          Fall 2020
+        </p>
+        <Button href="semester/fall2020">View</Button>
+        <Divider />
+        <p>
+          Spring 2020
+        </p>
+        <Button href="semester/spring2020">View</Button>
       </Router>
     );
   }
-}
+};
 
 export default HomeView;
