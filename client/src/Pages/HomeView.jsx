8b7e470e2741e507e5a28c2f463f7514bccb63ed
tjgoldblatt <tj@psyton.com>
2020-11-11 17:37:33 -0500

    Updated homepage view

--- client/src/Pages/HomeView.jsx
+++ client/src/Pages/HomeView.jsx
@@ -1,36 +1,63 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import {BrowserRouter as Router, Link, Route, Switch} from 'react-router-dom';
-import { Button, Typography, Divider } from 'antd';
-//const { Title, Paragraph, Text, Link } = Typography;
+
+import Card from '@material-ui/core/Card';
+import CardContent from '@material-ui/core/CardContent';
+import Typography from '@material-ui/core/Typography';
+import {CardHeader, Grid} from '@material-ui/core';
 
 export class HomeView extends React.Component {
   state = {};
-
   render() {
     return (
-      <Router>
-        <div>
-          <h2>UMass CICS Course Scheduler</h2>
-          <h2>Overview of Semesters</h2>
-        </div>
-        <p>
-          Spring 2021
-        </p>
-        <Button href="semester/spring2021">View</Button>
-        <Divider />
-        <p>
-          Fall 2020
-        </p>
-        <Button href="semester/fall2020">View</Button>
-        <Divider />
-        <p>
-          Spring 2020
-        </p>
-        <Button href="semester/spring2020">View</Button>
-      </Router>
+      <div>
+        <CardHeader title="Semesters:" align="center" />
+        <br />
+        <Grid
+          container
+          direction="row"
+          spacing="3"
+          alignItems="center"
+          justify="center"
+        >
+          <Grid item lg="3" alignItems="center">
+            <Card style={{maxWidth: '500px', maxHeight: '300px'}} elevation="2">
+              <CardContent>
+                <Typography style={{fontSize: '18px'}}>Spring 2020</Typography>
+              </CardContent>
+            </Card>
+          </Grid>
+
+          <Grid item lg="3">
+            <Card style={{maxWidth: '500px', maxHeight: '300px'}} elevation="2">
+              <CardContent>
+                <Typography style={{fontSize: '18px'}}>Fall 2020</Typography>
+              </CardContent>
+            </Card>
+          </Grid>
+
+          <Grid item lg="3">
+            <Link
+              to="/semester"
+              style={{color: 'white', textDecoration: 'none'}}
+            >
+              <Card
+                style={{maxWidth: '300px', maxHeight: '300px'}}
+                elevation="2"
+              >
+                <CardContent>
+                  <Typography style={{fontSize: '18px'}}>
+                    Spring 2021
+                  </Typography>
+                </CardContent>
+              </Card>
+            </Link>
+          </Grid>
+        </Grid>
+      </div>
     );
   }
-};
+}
 
 export default HomeView;
