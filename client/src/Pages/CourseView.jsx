f33e56d25e52630ff8f95e43721f3cc8ae897357
dhruvivora <dvora@umass.edu>
2020-11-10 23:27:22 -0500

    Update CourseView.jsx

--- client/src/Pages/CourseView.jsx
+++ client/src/Pages/CourseView.jsx
@@ -7,8 +7,53 @@ export class CourseView extends React.Component {
   render() {
     return (
       <div>
-        <h2>This is the CourseView webpage</h2>
+         <div>
+           <h2>Course Overview</h2>
+        </div>
+
+        <div>
+          <table>
+            <tr>
+              <th>Course Name: CS121</th>
+              <th></th>
+            </tr>
+  
+            <tr>
+              <td>Website</td>
+              <td>www.cs121.com</td>
+            </tr>
+  
+            <tr>
+              <td>Professor</td>
+              <td>Joe Chiu</td>
+            </tr>
+  
+            <tr>
+              <td>Rate My Professor Rating</td>
+              <td>4.2/5</td>
+            </tr>
+  
+            <tr>
+              <td>Credits</td>
+              <td>3</td>
+            </tr>
+ 
+            <tr>
+              <td>Prerequisites</td>
+              <td>None</td>
+            </tr>
+  
+            <tr>
+              <td>Course Description</td>
+              <td>Suspendisse ullamcorper vulputate hendrerit. Duis vestibulum convallis risus efficitur varius. Pellentesque tempor sem at risus vehicula porta. Proin arcu urna, eleifend quis orci sit amet, condimentum condimentum erat. Praesent dolor risus, venenatis eu lectus at, elementum tempus nunc. Fusce vitae lectus nec ligula accumsan mattis porttitor sit amet neque. Sed vel placerat lorem.</td>
+            </tr>
+
+          </table>
+        </div>
       </div>
+     
+
+      
     );
   }
 }
