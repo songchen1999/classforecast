ea9bbbfa480e196f32f89a8e5dc92117795ad152
dhruvivora <dvora@umass.edu>
2020-11-11 12:19:55 -0500

    fixed table using materials ui

--- client/src/Pages/CourseView.jsx
+++ client/src/Pages/CourseView.jsx
@@ -1,61 +1,21 @@
 import React from 'react';
 import {BrowserRouter as Router, Link, Route, Switch} from 'react-router-dom';
+import Mtab from '../Components/course';
 
 export class CourseView extends React.Component {
   state = {};
 
   render() {
     return (
-      <div>
-         <div>
-           <h2>Course Overview</h2>
-        </div>
-
-        <div>
-          <table>
-            <tr>
-              <th>Course Name: CS121</th>
-              <th></th>
-            </tr>
-  
-            <tr>
-              <td>Website</td>
-              <td>www.cs121.com</td>
-            </tr>
-  
-            <tr>
-              <td>Professor</td>
-              <td>Joe Chiu</td>
-            </tr>
-  
-            <tr>
-              <td>Rate My Professor Rating</td>
-              <td>4.2/5</td>
-            </tr>
-  
-            <tr>
-              <td>Credits</td>
-              <td>3</td>
-            </tr>
- 
-            <tr>
-              <td>Prerequisites</td>
-              <td>None</td>
-            </tr>
-  
-            <tr>
-              <td>Course Description</td>
-              <td>An Intro to CompSci.</td>
-            </tr>
-
-          </table>
-        </div>
+      <div style={{display: 'flex',
+        alignItems: 'center',
+        justifyContent: 'center',}}>
+        <Mtab/>
       </div>
-     
-
-      
+       
     );
   }
 }
 
 export default CourseView;
+
